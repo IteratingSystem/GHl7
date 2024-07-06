6f0eea73b4185b07ae28b41fdbfae60bc4be48c2
wenlong <midsu_wy@163.com>
2024-07-06 21:00:29 +0800

    创建项目

---
+++ core/src/main/java/com/ghl7/MainApplication.java
@@ -0,0 +1,31 @@
+package com.ghl7;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MainApplication extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
